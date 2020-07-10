52590859a31f999911847903501d9a599bde8512
AyanaZaire <ayanazaire@gmail.com>
2020-07-10 07:17:55 -0400

    implement jwt token

--- src/index.js
+++ src/index.js
@@ -6,8 +6,10 @@ document.addEventListener('DOMContentLoaded', () => {
   getSyllabi()
 
   const createSyllabusForm = document.querySelector("#create-syllabus-form")
-
   createSyllabusForm.addEventListener("submit", (e) => createFormHandler(e))
+
+  const loginForm = document.querySelector("#login-form")
+  loginForm.addEventListener("submit", (e) => loginFormHandler(e))
 })
 
 function getSyllabi() {
@@ -22,6 +24,46 @@ function getSyllabi() {
       document.querySelector('#syllabus-container').innerHTML += newSyllabus.renderSyllabusCard()
     })
   // .catch(err => console.log(err))
+  })
+}
+
+function loginFormHandler(e) {
+  e.preventDefault()
+  const emailInput = e.target.querySelector("#login-email").value
+  const pwInput = e.target.querySelector("#login-password").value
+  loginFetch(emailInput, pwInput)
+}
+
+function loginFetch(email, password){
+  const bodyData = {user: {
+        email: email,
+        password: password
+      }
+  }
+
+  fetch("http://localhost:3000/api/v1/login", {
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify(bodyData)
+  })
+  .then(response => response.json())
+  .then(json => {
+    localStorage.setItem('jwt_token', json.jwt)
+    renderUserProfile()
+  })
+}
+
+function renderUserProfile() {
+  console.log(localStorage.getItem('jwt_token'));
+  fetch('http://localhost:3000/api/v1/profile', {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem('jwt_token')}`
+    }
+  })
+  .then(response => response.json())
+  .then(json => {
+    alert(`Welcome back ${json.user.data.attributes.name}`)
   })
 }
 
